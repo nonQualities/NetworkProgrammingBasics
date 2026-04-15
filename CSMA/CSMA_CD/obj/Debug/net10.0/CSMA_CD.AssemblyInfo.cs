b64d9144c0c6232d12bfbe4b8f6c8b21db859ebf
Ronit Choudhury <ronit965choudhury@outlook.com>
2026-04-15 21:13:28 +0530

    ReadMe Added

--- CSMA/CSMA_CD/obj/Debug/net10.0/CSMA_CD.AssemblyInfo.cs
+++ CSMA/CSMA_CD/obj/Debug/net10.0/CSMA_CD.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSMA_CD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+401e523f9e1cab29f6ae618a74a30053031eae58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afa54efd5c63e4d122874998a3ac2e89c94c5262")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSMA_CD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSMA_CD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
